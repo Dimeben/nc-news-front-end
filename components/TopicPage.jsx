327d6dca3b03a1c023af7f6aa36fdab9bba1a8e4
Ben McCarthy <ben.mccarthy92@gmail.com>
2024-09-18 10:06:51 +0100

    12-error-handling

--- components/TopicPage.jsx
+++ components/TopicPage.jsx
@@ -2,24 +2,46 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ArticlesCard } from "./ArticlesCard";
 import { getAllArticles } from "../utils/api";
+import { Navigate } from "react-router-dom";
 
-export const TopicPage = () => {
+export const TopicPage = ({ allTopics }) => {
   const { topicslug } = useParams();
   const [filteredArticles, setFilteredArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const topicsLoaded = allTopics && allTopics.length > 0;
 
   useEffect(() => {
-    getAllArticles("created_at", "DESC", "100", "1", topicslug).then(
-      (response) => {
-        setFilteredArticles(response.articles);
-      }
-    );
-  }, [topicslug]);
+    if (topicsLoaded) {
+      setIsLoading(true);
+      getAllArticles("created_at", "DESC", "100", "1", topicslug).then(
+        (response) => {
+          setFilteredArticles(response.articles);
+          setIsLoading(false);
+        }
+      );
+    }
+  }, [topicslug, topicsLoaded]);
+
+  const validTopic = allTopics.some((topic) => topic.slug === topicslug);
+
+  if (!topicsLoaded) {
+    return <p>Loading...</p>;
+  }
+
+  if (!validTopic) {
+    return <Navigate to="/404" />;
+  }
 
   return (
     <section className="grid-container">
-      {filteredArticles.map((article) => {
-        return <ArticlesCard key={article.article_id} article={article} />;
-      })}
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        filteredArticles.map((article) => {
+          return <ArticlesCard key={article.article_id} article={article} />;
+        })
+      )}
     </section>
   );
 };
